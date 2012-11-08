57c931c7f8a26fc535c103d3c2577c19b6e08608
kvark <kvark@localhost>
2012-11-07 21:28:59 -0500

    Switched shaders to use LinearMap, implemented program binding and value read-backs.

--- engine/shade.rs
+++ engine/shade.rs
@@ -1,6 +1,6 @@
-extern mod std;
 extern mod glcore;
 extern mod lmath;
+use send_map::linear::LinearMap;
 
 
 pub type Location = glcore::GLint;
@@ -9,31 +9,30 @@ pub trait Uniform	{
 	fn load( loc : Location );
 }
 
+pub impl () : Uniform	{
+	fn load( _loc : Location )	{}
+}
 pub impl float : Uniform	{
 	fn load( loc : Location )	{
+		//io::println( fmt!("%d: %f",loc as int,self) );
 		glcore::glUniform1f( loc, self as glcore::GLfloat );
 	}
 }
-
 pub impl int : Uniform	{
 	fn load( loc : Location )	{
 		glcore::glUniform1i( loc, self as glcore::GLint );
 	}
 }
-
-
 pub impl lmath::vector::vec4 : Uniform	{
 	fn load( loc : Location )	{
 		glcore::glUniform4fv( loc, 4, self.to_ptr() );
 	}
 }
-
 pub impl lmath::vector::ivec4 : Uniform	{
 	fn load( loc : Location )	{
 		glcore::glUniform4iv( loc, 4, self.to_ptr() );
 	}
 }
-
 pub impl lmath::quaternion::quat4 : Uniform	{
 	fn load( loc : Location )	{
 		glcore::glUniform4fv( loc, 4, self.to_ptr() );
@@ -45,7 +44,7 @@ struct Parameter	{
 	loc		: Location,
 	storage	: glcore::GLenum,
 	size	: glcore::GLint,
-	value	: @Uniform	//FIXME (wait for Rust)
+	mut value	: @Uniform	//FIXME (wait for Rust)
 }
 
 struct Attribute	{
@@ -54,12 +53,44 @@ struct Attribute	{
 	size	: glcore::GLint
 }
 
-impl @Attribute : Copy	{
+impl Parameter	{
+	fn read( h : glcore::GLuint )-> bool	{
+		if self.storage == glcore::GL_FLOAT	{
+			unsafe	{
+				let mut v = 0f32;
+				glcore::glGetUniformfv( h, self.loc, ptr::addr_of(&v) );
+				self.value = (v as float) as @Uniform;
+			}
+		}else
+		if self.storage == glcore::GL_INT	{
+			unsafe	{
+				let mut v = 0i32;
+				glcore::glGetUniformiv( h, self.loc, ptr::addr_of(&v) );
+				self.value = (v as int) as @Uniform;
+			}
+		}else
+		if self.storage == glcore::GL_FLOAT_VEC4	{
+			unsafe	{
+				let mut v = lmath::vector::Vec4::new(0f32,0f32,0f32,0f32);
+				glcore::glGetUniformfv( h, self.loc, v.to_ptr() );
+				self.value = @v as Uniform;
+			}
+		}else
+		if self.storage == glcore::GL_INT_VEC4	{
+			unsafe	{
+				let mut v = lmath::vector::Vec4::new(0i32,0i32,0i32,0i32);
+				glcore::glGetUniformiv( h, self.loc, v.to_ptr() );
+				self.value = @v as Uniform;
+			}
+		}else	{return false;}
+		true
+	}
 }
 
-pub type AttriMap	= std::map::HashMap<~str,@Attribute>;
-pub type ParaMap	= std::map::HashMap<~str,@Parameter>;
-pub type DataMap	= std::map::HashMap<~str,@Uniform>;
+
+pub type AttriMap	= LinearMap<~str,@Attribute>;
+pub type ParaMap	= LinearMap<~str,@Parameter>;
+pub type DataMap	= LinearMap<~str,@Uniform>;
 
 pub struct Shader	{
 	handle	: glcore::GLuint,
@@ -78,10 +109,15 @@ pub struct Program	{
 	info	: ~str,
 	attribs	: AttriMap,
 	params	: ParaMap,
+
+	drop	{
+		// assert: not current
+		glcore::glDeleteProgram( self.handle );
+	}
 }
 
 
-pub fn createShader( target : glcore::GLenum, code : &str )-> Shader	{
+pub fn create_shader( target : glcore::GLenum, code : &str )-> Shader	{
 	let h = glcore::glCreateShader( target );
 	let mut length = code.len() as glcore::GLint;
 	do str::as_c_str(code) |text|	{
@@ -110,8 +146,7 @@ pub fn createShader( target : glcore::GLenum, code : &str )-> Shader	{
 }
 
 
-fn queryAttributes( h: glcore::GLuint )-> AttriMap	{
-	let mut rez = std::map::HashMap::<~str,@Attribute>();
+fn query_attributes( h: glcore::GLuint )-> AttriMap	{
 	let mut num		= 0 as glcore::GLint;
 	let mut max_len	= 0 as glcore::GLint;
 	let mut raw_bytes	: *libc::c_char;
@@ -121,6 +156,7 @@ fn queryAttributes( h: glcore::GLuint )-> AttriMap	{
 		let info_bytes = vec::from_elem( max_len as uint, 0 as libc::c_char );
 		raw_bytes = vec::raw::to_ptr(info_bytes);
 	}
+	let mut rez		= send_map::linear::linear_map_with_capacity::<~str,@Attribute>( num as uint );
 	while num>(0 as glcore::GLint)	{
 		num -= 1;
 		let mut length	= 0 as glcore::GLint;
@@ -140,8 +176,7 @@ fn queryAttributes( h: glcore::GLuint )-> AttriMap	{
 }
 
 
-fn queryParameters( h : glcore::GLuint )-> ParaMap	{
-	let mut rez = std::map::HashMap::<~str,@Parameter>();
+fn query_parameters( h : glcore::GLuint )-> ParaMap	{
 	let mut num		= 0 as glcore::GLint;
 	let mut max_len	= 0 as glcore::GLint;
 	let mut raw_bytes	: *libc::c_char;
@@ -151,6 +186,8 @@ fn queryParameters( h : glcore::GLuint )-> ParaMap	{
 		let info_bytes = vec::from_elem( max_len as uint, 0 as libc::c_char );
 		raw_bytes = vec::raw::to_ptr(info_bytes);
 	}
+	let mut rez		= send_map::linear::linear_map_with_capacity::<~str,@Parameter>( num as uint );
+	let init_value	= @() as Uniform;
 	while num>(0 as glcore::GLint)	{
 		num -= 1;
 		let mut length	= 0 as glcore::GLint;
@@ -164,14 +201,15 @@ fn queryParameters( h : glcore::GLuint )-> ParaMap	{
 			name = str::raw::from_c_str( raw_bytes );
 		}
 		let location = glcore::glGetUniformLocation( h, raw_bytes );
-		let value = @1 as Uniform;
-		rez.insert( name, @Parameter{ loc:location, storage:storage, size:size, value:value } );
+		let mut p = @Parameter{ loc:location, storage:storage, size:size, value:init_value };
+		p.read( h );
+		rez.insert( name, p );
 	}
 	rez
 }
 
 
-pub fn createProgram( shaders : ~[Shader] )-> Program	{
+pub fn create_program( shaders : ~[Shader] )-> Program	{
 	let h = glcore::glCreateProgram();
 	for shaders.each |s| {
 		glcore::glAttachShader( h, s.handle );
@@ -195,12 +233,40 @@ pub fn createProgram( shaders : ~[Shader] )-> Program	{
 	}
 	// done
 	Program{ handle:h, alive:ok, info:message,
-		attribs	:queryAttributes(h),
-		params	:queryParameters(h) }
+		attribs	:query_attributes(h),
+		params	:query_parameters(h) }
 }
 
+
+pub fn createData()-> ~mut DataMap	{
+	~mut LinearMap::<~str,@Uniform>()
+}
+
+
 impl Program	{
-	fn bind()	{
+	fn bind( data : &const DataMap )	{
 		glcore::glUseProgram( self.handle );
+		/* FIXME: each_const
+		for data.each |name,value|	{
+			match self.params.find(name)	{
+				Some(ref par) => {
+					//FIXME:
+					//if par.value != *value {
+						par.value = *value;
+						value.load( par.loc );
+					//}
+				}
+				None => io::println( fmt!("Param not found: %s", *name) )
+			}
+		}*/
+		for self.params.each |name,par|	{
+			match data.find(name)	{
+				Some(value) =>	{
+					par.value = value;
+					value.load( par.loc );
+				}
+				None => ()
+			}
+		}
 	}
 }
--- game/main.rs
+++ game/main.rs
@@ -1,16 +1,17 @@
 extern mod glfw3;
 extern mod glcore;
 extern mod engine;
-
+use send_map::linear::LinearMap;
 
 type handle = u32;
 
 struct Sample	{
 	program	: engine::shade::Program,
+	data	: ~mut engine::shade::DataMap,
 	buffer	: handle
 }
 
-fn genBuffer() -> handle	{
+fn gen_buffer() -> handle	{
 	let mut h = 0u32;
 	unsafe	{
 		glcore::glGenBuffers( 1, ptr::addr_of(&h) );
@@ -21,13 +22,15 @@ fn genBuffer() -> handle	{
 fn init() -> Sample	{
 	// load shaders
 	let vert_code = "attribute vec2 position; void main()	{ gl_Position = vec4(position,0.0,1.0); }";
-	let frag_code = "void main()	{ gl_FragData[0] = vec4(1.0,0.0,0.0,1.0); }";
-	let vert_shader = engine::shade::createShader( glcore::GL_VERTEX_SHADER, vert_code );
-	let frag_shader = engine::shade::createShader( glcore::GL_FRAGMENT_SHADER, frag_code );
-	let program = engine::shade::createProgram( ~[vert_shader,frag_shader] );
+	let frag_code = "uniform float color=0.5; void main()	{ gl_FragData[0] = vec4(color,0.0,0.0,1.0); }";
+	let vert_shader = engine::shade::create_shader( glcore::GL_VERTEX_SHADER, vert_code );
+	let frag_shader = engine::shade::create_shader( glcore::GL_FRAGMENT_SHADER, frag_code );
+	let program = engine::shade::create_program( ~[vert_shader,frag_shader] );
+	//let name = ~"color";
+	//io::println( fmt!("Initial val: %f",program.params.get(name).value as float));
 	// load buffers
 	let vdata = [-1f32,-1f32,0f32,1f32,1f32,-1f32];
-	let buf_handle = genBuffer();
+	let buf_handle = gen_buffer();
 	glcore::glBindBuffer( glcore::GL_ARRAY_BUFFER, buf_handle );
 	unsafe	{
 		glcore::glBufferData( glcore::GL_ARRAY_BUFFER,
@@ -37,16 +40,17 @@ fn init() -> Sample	{
 	}
 	// done
 	io::println( fmt!("Init: program %u, buffer %u",program.handle as uint,buf_handle as uint) );
-	Sample {program:program, buffer:buf_handle}
+	Sample { program:program, data:engine::shade::createData(), buffer:buf_handle }
 }
 
 
-fn render(s:&Sample) ->bool	{
+fn render( s : &Sample ) ->bool	{
 	glcore::glClearColor( 0.5f32, 0.5f32, 1.0f32, 1.0f32 );
 	glcore::glClearDepth( 1.0f64 );
 	glcore::glClear( glcore::GL_COLOR_BUFFER_BIT | glcore::GL_DEPTH_BUFFER_BIT );
 	
-	s.program.bind();
+	s.data.insert( ~"color", @1f as engine::shade::Uniform );
+	s.program.bind(	s.data );
 	glcore::glBindBuffer( glcore::GL_ARRAY_BUFFER, s.buffer );
 	glcore::glVertexAttribPointer( 0, 2, glcore::GL_FLOAT, glcore::GL_FALSE,
 		sys::size_of::<f32>()*2u as i32, 0 as *libc::c_void );
