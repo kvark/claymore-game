31c4b6eed8313debd3a885d8d3b2e87e03968af4
Dzmitry Malyshau <Dzmitry Malyshau@localhost>
2013-08-23 12:09:29 -0400

    Moving things around again

--- engine/engine.rs
+++ engine/engine.rs
@@ -11,17 +11,23 @@ extern mod stb_image;
 extern mod lmath;
 extern mod openal;
 
+pub mod gr_low	{
+	pub mod buf;
+	pub mod context;
+	pub mod frame;
+	pub mod rast;
+	pub mod shade;
+	pub mod texture;
+}
+
+pub mod gr_mid	{
+	pub mod call;
+	pub mod draw;
+	pub mod font;
+	pub mod mesh;
+}
+
 pub mod anim;
 pub mod audio;
-pub mod buf;
-pub mod call;
-pub mod context;
-pub mod draw;
-pub mod font;
-pub mod frame;
 pub mod load;
-pub mod mesh;
-pub mod rast;
-pub mod shade;
 pub mod space;
-pub mod texture;
